13bd22dcbde19a33c061a41385b8694d38940773
TheLilGrant <60147677+TheLilGrant@users.noreply.github.com>
2020-04-07 16:36:10 -0400

    Comments

--- src/COEN346_A3/Main.java
+++ src/COEN346_A3/Main.java
@@ -17,20 +17,27 @@ import static java.lang.String.format;
 
 class sharedQueues {
 
+    //These queues are accessible by the two CPU cores and contain the processes to be run
     static Queue<Process> waitingQueue = new LinkedList<>();
     static Queue<Process> readyQueue = new LinkedList<>();
+
+    //This Queue contains the list of commands from commands.txt
     static Queue<Command> commandQueue = new LinkedList<>();
+
+    //This value will hold the system time at start of execution
     static long start;
 
 }
 
 public class Main {
 
+    //These are temporary arraylists for the purpose of sorting before transferring to a queue
     static ArrayList<Process> waitingList = new ArrayList<>();
     static ArrayList<Command> commandList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
+        //A semaphore that only allows one core at a time to access certain areas
         Semaphore cpuLock = new Semaphore(1);
 
         //Start by getting the input file for processes
@@ -55,21 +62,23 @@ public class Main {
             processCount++;
         }
 
+        //Take the commands.txt info and read it
         File commandsFile = new File(Paths.get("commands.txt").toAbsolutePath().toString());
         BufferedReader commandsInput = new BufferedReader(new FileReader(commandsFile));
 
+        //To hold the commands during reading
         String[] commandsArray;
         String commandsString;
 
         while ((commandsString = commandsInput.readLine()) != null) {
-            commandsArray = commandsString.split("\\s", 3); //split arrival time and burst time
-            if (commandsArray.length < 3) {
+            commandsArray = commandsString.split("\\s", 3); //Split the command name, value and variableID
+            if (commandsArray.length < 3) { //If there are less than three strings, LOOKUP or RELEASE commands
                 commandList.add(
                         new Command(
                                 Integer.parseInt(commandsArray[1].trim()), //The variable the command is dealing with
                                 commandsArray[0].trim())); //The type of command
             }
-            else {
+            else { //if there are 3 strings in the array (A STORE command)
                 commandList.add(
                         new Command(
                                 Integer.parseInt(commandsArray[1].trim()), //The variable the command is dealing with
@@ -89,23 +98,29 @@ public class Main {
         //begin process execution/simulation
         System.out.println("================Process Execution=================");
 
+
+        //Add the sorted by arrival processes in Waiting List to the Waiting Queue
         for (int m = 0; m < waitingList.size(); m++){
 
             sharedQueues.waitingQueue.add(waitingList.get(m));
 
         }
 
+        //Add the commands read from commands.txt to a queue for easier retrieval by processes
         for (int m = 0; m < commandList.size(); m++){
 
             sharedQueues.commandQueue.add(commandList.get(m));
 
         }
 
-
-
+        //Create two instances of scheduler, these will act as our CPU cores cpu0 and cpu1
         Scheduler cpu0 = new Scheduler(new Process(0, 0, 0), cpuLock, "cpu0");
         Scheduler cpu1 = new Scheduler(new Process(1, 0, 0), cpuLock, "cpu1");
+
+        //Set the start time of the simulation
         sharedQueues.start = System.nanoTime();
+
+        //Start both cores
         cpu0.start();
         cpu1.start();
 
@@ -120,16 +135,16 @@ public class Main {
             this.sem = sem;
             this.name = name;
         }
-        private void oneRoundRobinRound(Process p){
 
+        private void oneRoundRobinRound(Process p){ //One round of execution
 
             if (p.getRunTime() <= 3) {
 
-                if(p.getRunTime() == p.getTotalRunTime()){
-                    p.setStatus(ProcessStatus.STARTED); //resume the process
+                if(p.getRunTime() == p.getTotalRunTime()){ //If this is the first time the process has run
+                    p.setStatus(ProcessStatus.STARTED); //Start the process
                     printProcessStatus(p);
                 }
-                p.setStatus(ProcessStatus.RESUMED); //resume the process
+                p.setStatus(ProcessStatus.RESUMED); //Resume the process
                 printProcessStatus(p);
                 executeProcessComplete(p);
                 p.setStatus(ProcessStatus.FINISHED);
@@ -137,11 +152,11 @@ public class Main {
 
             } else {
 
-                if(p.getRunTime() == p.getTotalRunTime()){
-                    p.setStatus(ProcessStatus.STARTED); //resume the process
+                if(p.getRunTime() == p.getTotalRunTime()){ //If this is the first time the process has run
+                    p.setStatus(ProcessStatus.STARTED); //Start the process
                     printProcessStatus(p);
                 }
-                p.setStatus(ProcessStatus.RESUMED); //resume the process
+                p.setStatus(ProcessStatus.RESUMED); //Resume the process
                 printProcessStatus(p);
                 executeProcess(p);
                 p.setStatus(ProcessStatus.PAUSED);
@@ -149,7 +164,6 @@ public class Main {
                 sharedQueues.readyQueue.add(p);
 
             }
-
         }
 
         private void printProcessStatus(Process p){
@@ -157,7 +171,6 @@ public class Main {
             System.out.println(name + ": Clock: " + getCurrentTime() + ", Process "
                     + p.getPID() + ", " + p.getStatus()
                     + ", remaining time " + format("%.3f",p.getRunTime()));
-
 
         }
 
@@ -180,7 +193,7 @@ public class Main {
             long executionStart = System.nanoTime();
             MemoryManager memManager = new MemoryManager();
 
-            while (executionStart - sharedQueues.start < 3000000){
+            while (System.nanoTime() - executionStart < 3000000){
 
                 Command command = sharedQueues.commandQueue.remove();
                 if (command.getCommandType() == CommandType.STORE){
@@ -201,26 +214,28 @@ public class Main {
         }
 
         private void executeProcessComplete(Process p){
-            long executionStart = System.nanoTime();
-            MemoryManager memManager = new MemoryManager();
+//            long executionStart = System.nanoTime();
+//            MemoryManager memManager = new MemoryManager();
+//
+//            while (executionStart - sharedQueues.start < (p.getRunTime() * 1000000000)){
+//
+//                Command command = sharedQueues.commandQueue.remove();
+//                if (command.getCommandType() == CommandType.STORE){
+//                    memManager.memStore(command.getCommandVariable(), command.getCommandValue());
+//                }
+//                else if (command.getCommandType() == CommandType.LOOKUP){
+//                    memManager.memLookup(command.getCommandVariable());
+//                }
+//                else if (command.getCommandType() == CommandType.RELEASE){
+//                    memManager.memFree(command.getCommandVariable());
+//                }
+//                sharedQueues.commandQueue.add(command);
+//
+//            }
+//
+//            p.setRunTime(0);
 
-            while (executionStart - sharedQueues.start < (p.getRunTime() * 1000000000)){
 
-                Command command = sharedQueues.commandQueue.remove();
-                if (command.getCommandType() == CommandType.STORE){
-                    memManager.memStore(command.getCommandVariable(), command.getCommandValue());
-                }
-                else if (command.getCommandType() == CommandType.LOOKUP){
-                    memManager.memLookup(command.getCommandVariable());
-                }
-                else if (command.getCommandType() == CommandType.RELEASE){
-                    memManager.memFree(command.getCommandVariable());
-                }
-                sharedQueues.commandQueue.add(command);
-
-            }
-
-            p.setRunTime(0);
 
         }
 
