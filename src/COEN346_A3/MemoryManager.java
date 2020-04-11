ee9b64c2eb381506c6da61491a19456c22cf07b9
TheLilGrant <60147677+TheLilGrant@users.noreply.github.com>
2020-04-11 02:59:27 -0400

    Removed Debugging Code

--- src/COEN346_A3/MemoryManager.java
+++ src/COEN346_A3/MemoryManager.java
@@ -26,13 +26,11 @@ public class MemoryManager{
 
         for (int i = 0; i < this.memorySize; i++) {
             RAM.add(new Frame(-1, -1));
-            System.out.println("ADDING A FRAME TO RAM");
         }
     }
 
     public void memStore(int variableID, int value) throws IOException, InterruptedException {
         //first we check if the RAM is full
-        System.out.println(RAM.size());
         if(usedMemory == memorySize) {//ram is full
             System.out.println("RAM is full!"); //todo: remove this, used for debugging
             System.out.println("Holding Variable " + RAM.get(0).getID() + " and Variable " + RAM.get(1).getID());
@@ -40,20 +38,17 @@ public class MemoryManager{
 
             //if the ram is full we can just open the vm.txt and append the new variable to the end
             fileSemaphore.acquire();
-            System.out.println("File Semaphore Acquired");
             File file = new File(Paths.get("vm.txt").toAbsolutePath().toString());
             Writer output = new BufferedWriter(new FileWriter(file, true));
             output.append(variableID + " " + value + " " + System.currentTimeMillis() + "\n");
             output.close();
             fileSemaphore.release();
-            System.out.println("File Semaphore Released");
         }
         else { //there is space in the ram
             usedMemory++;
             //we have to find where we can put the new variable
             //if the id is -1 then we can add it there as a id of -1 means its empty/free
             ramSemaphore.acquire();
-            System.out.println("RAM Semaphore Acquired");
             for (int i = 0; i < RAM.size(); i ++) {
                 if(RAM.get(i).getID() == -1) {//if that point is free
                     RAM.get(i).setID(variableID);
@@ -63,7 +58,6 @@ public class MemoryManager{
                 }
             }
             ramSemaphore.release();
-            System.out.println("RAM Semaphore Released");
         }
 
 
@@ -76,10 +70,9 @@ public class MemoryManager{
         int indexToSwap = 0;
         Frame tmpFrame = new Frame(-1, -1); //assign -1 as a temporary/invalid ID
         ramSemaphore.acquire(); //stop others from reading and writing to the file
-        System.out.println("RAM Semaphore Acquired");
-        System.out.println(RAM.size());
+
         tmpSmallestAccessTime = RAM.get(0).getAccessTime();
-        System.out.println(RAM.get(0).getID());
+
         for (int i = 1; i < RAM.size(); i++) {
             if(RAM.get(i).getAccessTime() < tmpSmallestAccessTime){
                 tmpFrame = RAM.get(i);
@@ -92,14 +85,12 @@ public class MemoryManager{
         }
 
         ramSemaphore.release();
-        System.out.println("RAM Semaphore Released");
 
         //now that we've gone through the entire RAM, we know the ID of what we need to swap and we've
         //stored it as a tmp frame. Now we can add the new variable to ram. Then we write the old one to vm.txt
 
         //Start by getting the input file and creating an array consisting of all the vm elements
         fileSemaphore.acquire();
-        System.out.println("File Semaphore Acquired");
         File file = new File(Paths.get("vm.txt").toAbsolutePath().toString());
         /*BufferedReader in = new BufferedReader(new FileReader(file));
         //Read data from file and populate the virtual memory
@@ -121,10 +112,8 @@ public class MemoryManager{
         */
         Writer output = new BufferedWriter(new FileWriter(file, true));
         output.append(tmpFrame.ID + " " + tmpFrame.value + " " + tmpFrame.accessTime + "\n");
-        System.out.println(tmpFrame.ID);
         output.close();
         fileSemaphore.release();
-        System.out.println("File Semaphore Released");
 
         //now that the old variable is in the virtual memory, we can write the new variable to ram
         System.out.println("SWAP Variable " + varID + " with Variable " + tmpFrame.ID); //todo: make sure these are the right values
@@ -143,7 +132,6 @@ public class MemoryManager{
         boolean erased = false;
         //first we're going to look through the ram and see if the variable exists
         ramSemaphore.acquire();
-        System.out.println("RAM Semaphore Acquired");
         for (int i = 0; i < RAM.size(); i++) {
             if(RAM.get(i).getID() == variableID){ //if tis there, erase (assign values of -1) the variable
                 RAM.get(i).setValue(-1);
@@ -152,12 +140,10 @@ public class MemoryManager{
             }
         }
         ramSemaphore.release();
-        System.out.println("RAM Semaphore Released");
         //if not in ram
         if (!erased){
             //now we have to check the disk
             fileSemaphore.acquire();
-            System.out.println("File Semaphore Acquired");
             File file = new File(Paths.get("vm.txt").toAbsolutePath().toString());
             BufferedReader in = new BufferedReader(new FileReader(file));
             ArrayList<Frame> virtualMemory = new ArrayList<>();
@@ -186,7 +172,6 @@ public class MemoryManager{
             }
             output.close();
             fileSemaphore.release();
-            System.out.println("File Semaphore Released");
 
         }
         //if not in disk either
@@ -201,16 +186,13 @@ public class MemoryManager{
                 return f.getValue();
             }
         }
-        System.out.println("TESTTESTESTEST");
         //if its not in the ram we must look for it in the vm.txt
         fileSemaphore.acquire();
-        System.out.println("File Semaphore Acquired");
         File file = new File(Paths.get("vm.txt").toAbsolutePath().toString());
         BufferedReader in = new BufferedReader(new FileReader(file));
         ArrayList<Frame> virtualMemory = new ArrayList<>();
         String[] splitInputArray;
         String str;
-        System.out.println("TEST2");
         while ((str = in.readLine()) != null) {
             splitInputArray = str.split("\\s", 3); //split variable ID, value, and access time
             virtualMemory.add(new Frame(Integer.parseInt(splitInputArray[0].trim()),
@@ -218,23 +200,17 @@ public class MemoryManager{
                     Long.parseLong(splitInputArray[2].trim())));
         }
         in.close();
-        System.out.println("TEST3");
-        System.out.println(virtualMemory.size());
+
         for (Frame f: virtualMemory) {
-            System.out.println("TEST4");
             if(f.getID() == variableID) {
                 fileSemaphore.release(); //release it so moveOldestFrame can acquire it
-                System.out.println("File Semaphore Released");
                 moveOldestFrameToVirtualMemory(variableID, f.getValue());
-                System.out.println("TEST6");
                 System.out.println("LOOKUP: The value is in Virtual Memory");
                 return f.getValue();
 
             }
         }
-        System.out.println("TEST4");
         fileSemaphore.release(); //release it down here because we dont want the contents of vm.txt to change while without the array list changing
-        System.out.println("File Semaphore Released");
 
         //finally, if its not in ram or vm we return -1;
         System.out.println("LOOKUP: The value is NOWHERE TO BE FOUND");
